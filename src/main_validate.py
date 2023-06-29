485f8b5f67472d65aea3405a8d29926021296d36
Dewey Gaedcke <dewey@pathoz.com>
2023-06-29 14:32:06 -0500

    vers bump & validate

--- src/main_validate.py
+++ src/main_validate.py
@@ -4,26 +4,20 @@
 # google_module = sys.modules['google']
 # google_module.__path__.append(google_path)
 
-import six
-from google.cloud.ndb import model
+# import six
+# from google.cloud.ndb import model
 from ts_shared_py3 import *
 from ts_shared_py3.models import *
 from ts_shared_py3.enums import *
 from ts_shared_py3.schemas import *
 from ts_shared_py3.wrappers import *
 
-# from ts_shared_py3.scoring import commBehImpactConsenus
+from ts_shared_py3.scoring import *
 
 # from ts_shared_py3.scoring import *
 from ts_shared_py3.scoring import *
 from ts_shared_py3.services import *
-from ts_shared_py3.config.behavior.load_yaml import BehaviorSourceSingleton
-
-# from ts_shared_py3.common import enums
-# from ts_shared_py3.common import config
-# from ts_shared_py3.common import models  # non-scoring models
-# from ts_shared_py3.common import schemas
-# from ts_shared_py3.common import utils
+from ts_shared_py3.config.behavior.load_yaml import *
 
 # to look for errors or import probs, run this:
 #  python src/main_validate.py
